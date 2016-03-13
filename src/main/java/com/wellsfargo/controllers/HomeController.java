2da772275defa4f7d89245834bd3557d6fe77a08
Aditya <aditya05khajuria@gmail.com>
2016-03-13 10:00:48 +0530

    Initial commit

---
+++ src/main/java/com/wellsfargo/controllers/HomeController.java
@@ -0,0 +1,13 @@
+package com.wellsfargo.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "Welcome to the home page!";
+    }
+}
